3ae01c4880467b62d3868010ce0071468bf6cfcf
Nhật Phúc <88922817+HuynhDoNhatPhuc@users.noreply.github.com>
2022-04-03 10:17:23 +0700

    update

--- wp-content/themes/flatsome/functions.php
+++ wp-content/themes/flatsome/functions.php
@@ -11,3 +11,10 @@ require get_template_directory() . '/inc/init.php';
  * Note: It's not recommended to add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
  * Learn more here: http://codex.wordpress.org/Child_Themes
  */
+function my_custom_translations( $strings ) {
+$text = array(
+'Quick View' => 'Information'
+);
+$strings = str_ireplace( array_keys( $text ), $text, $strings );
+return $strings;
+}
